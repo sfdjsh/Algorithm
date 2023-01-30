45e87878e12168bfdd5d170d867d74dc429dc76e
sfdjsh <sfdjsh@naver.com>
2023-01-30 13:11:19 +0900

    알고리즘

---
+++ BAEKJOON/동전 1/BJ_2293.py
@@ -0,0 +1,12 @@
+n, k = map(int, input().split())
+coins = list(int(input()) for _ in range(n))
+
+dp = [0] * (k+1)
+dp[0] = 1
+
+for i in coins:
+    for j in range(i, k+1):
+        if j - i >= 0:
+            dp[j] += dp[j-i]
+
+print(dp[k])
