33d039338e1922ba6ce1629cfedb69b6e076e62c
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2023-07-23 02:10:47 +0900

    [level 2] Title: 예상 대진표, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12985. 예상 대진표/예상 대진표.py
@@ -0,0 +1,12 @@
+def solution(n,a,b):
+    answer = 0
+    
+    while True:
+        if a == b:
+            break
+        
+        a = (a + 1) // 2
+        b = (b + 1) // 2
+        answer += 1
+
+    return answer
