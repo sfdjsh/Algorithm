22014676a3bbc0012a5cef7690a97f85cbfc6b9a
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2023-08-09 01:48:01 +0900

    [Gold IV] Title: Puyo Puyo, Time: 184 ms, Memory: 116436 KB -BaekjoonHub

--- 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
+++ 백준/Gold/11559. Puyo Puyo/Puyo Puyo.py
@@ -2,17 +2,18 @@ from collections import deque
 
 di, dj = ((-1, 1, 0, 0), (0, 0, -1, 1))
 
+# bfs 함수
 def bfs(sx, sy, color):
-
     tmp = 0
+    
     lst = []
+    lst.append([sx, sy])
+    
     visited = [[0] * 6 for _ in range(12)]
     visited[sx][sy] = 1
 
     q = deque()
     q.append((sx, sy))
-    lst.append([sx, sy])
-
     while q:
         x, y = q.popleft()
         for i in range(4):
@@ -22,14 +23,14 @@ def bfs(sx, sy, color):
                 q.append((dx, dy))
                 lst.append([dx, dy])
 
-
-    if len(lst) >= 4:
+    if len(lst) >= 4:           # 같은 뿌요가 4개 이상일 때
         tmp = 1
         for i, j in lst:
-            puyo[i][j] = '.'
+            puyo[i][j] = '.'    # 뿌요 터트리기
         return tmp
 
-def delete():
+# 터진 뿌요 자리에, 위에서 아래로 떨어뜨리기
+def down():
     for k in range(6):
         for i in range(11, -1, -1):
             for j in range(i - 1, -1, -1):
@@ -39,25 +40,24 @@ def delete():
                     break
 
 answer = 0
+
 puyo = []
 for _ in range(12):
     arr = list(input())
     puyo.append(arr)
 
 while True:
-    flag = False
-
+    flag = False    # 연쇄 판별 변수
     for i in range(11, -1, -1):
         for j in range(6):
             if puyo[i][j] != '.':
-                if bfs(i, j, puyo[i][j]) == 1:
+                if bfs(i, j, puyo[i][j]) == 1:  # 색깔 뿌요 bfs 실행
                     flag = True
 
-
-    if flag:
+    if flag:            # 터진 뿌요가 있을 때
         answer += 1
-        delete()
-    else:
+        down()          # 뿌요 내리기
+    else:               # 터진 뿌요가 없으면 while문 종료
         break
 
 print(answer)
