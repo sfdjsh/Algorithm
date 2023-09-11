8f7a93210008bacdb2bdcb7e94622f526d1c9997
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2023-09-11 22:25:05 +0900

    [unrated] Title: 대충 만든 자판, Time: 1.17 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/unrated/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,23 +1,27 @@
 def solution(keymap, targets):
     answer = []
     
-    for target in targets:
-        result = 0
-        flag = True
-        for alpha in target:
-            cnt = 100
-            for key in keymap:
-                if alpha in key:
-                    tmp = key.index(alpha) + 1
-                    cnt = min(tmp, cnt)
-            if cnt == 100:
-                answer.append(-1)
-                flag = False
-                break
+    alpha = {}
+    
+    for key in keymap:
+        for i, a in enumerate(key):
+            if a in alpha:
+                alpha[a] = min(alpha[a], i + 1)
             else:
-                result += cnt
-                
-        if flag and result != 0:
-            answer.append(result)
+                alpha[a] = i + 1
+    
+    for target in targets:
+        cnt = 0
+        for t in target:
+            if t in alpha:
+                cnt += alpha[t]
+            else:
+                cnt = -1
+                break
+        
+        if cnt < 0:
+            answer.append(-1)
+        else:
+            answer.append(cnt)
     
     return answer
