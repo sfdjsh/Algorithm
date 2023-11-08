8bd6348268fa3a7a4858f8b86cb30fb8d6b8e9a6
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2023-11-08 14:35:19 +0900

    [level 1] Title: 예산, Time: 0.15 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,16 @@
+function solution(d, budget) {
+    var answer = 0;
+    var sum = 0;
+    
+    d.sort((a, b) => a - b)
+    for (let i = 0; i < d.length; i++) {
+        answer++;
+        sum += d[i];
+        
+        if (sum > budget) {
+            answer--;
+        }
+    }
+    
+    return answer;
+}
