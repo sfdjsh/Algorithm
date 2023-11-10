b383355ee8022f758cda34b3f8f34dfaefa1160d
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2023-11-10 15:18:04 +0900

    [level 1] Title: 행렬의 덧셈, Time: 9.09 ms, Memory: 67.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/12950. 행렬의 덧셈/행렬의 덧셈.js
@@ -0,0 +1,14 @@
+function solution(arr1, arr2) {
+    var answer = [[]];
+    
+    let lenX = arr1.length;
+    let lenY = arr1[0].length;
+    
+    for (let i = 0; i < lenX; i++) {
+        answer[i] = [];
+        for (let j = 0; j < lenY; j++) {
+            answer[i].push(arr1[i][j] + arr2[i][j])
+        }
+    }
+    return answer;
+}
