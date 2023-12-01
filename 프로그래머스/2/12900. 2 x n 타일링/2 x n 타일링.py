a73d22a01d044ea25acb80ab6f52a805e60ef52d
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2023-12-01 19:59:52 +0900

    [level 2] Title: 2 x n 타일링, Time: 11.90 ms, Memory: 11.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12900. 2 x n 타일링/2 x n 타일링.py
@@ -0,0 +1,14 @@
+def solution(n):
+    answer = 0
+    
+    if n == 1:
+        return 1
+    elif n == 2:
+        return 2
+    
+    dp = [0] * (n + 1)
+    dp[1], dp[2] = 1, 2
+    for i in range(3, n + 1):
+        dp[i] = (dp[i - 2] + dp[i - 1]) % 1000000007
+
+    return dp[n]
