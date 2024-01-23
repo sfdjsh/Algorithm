099b53fcd8e5d49c43a33dd1874d243c843334c6
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2024-01-23 13:19:41 +0900

    [level 2] Title: [1차] 캐시, Time: 0.76 ms, Memory: 10.4 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/README.md
+++ 프로그래머스/2/17680. ［1차］ 캐시/README.md
@@ -1,10 +1,10 @@
 # [level 2] [1차] 캐시 - 17680 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/17680?language=python3) 
 
 ### 성능 요약
 
-메모리: 10.4 MB, 시간: 0.72 ms
+메모리: 10.4 MB, 시간: 0.76 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 1월 1일 17:16:20
+2024년 1월 2일 13:19:38
 
 ### 문제 설명
 
--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.py
@@ -1,22 +1,20 @@
 def solution(cacheSize, cities):
     answer = 0
     
+    if cacheSize == 0:
+        return 5 * len(cities)
+    
+    cities = [c.lower() for c in cities]
     cache = []
-    for c in cities:
-        c = c.lower()       # 대소문자 구별 x                                       
-        
-        if cacheSize:                       # 캐시크기가 있을 때
-            if c not in cache:              # cache miss일 때 (도시 이름이 캐시 안에 없을 때) 
-                if len(cache) == cacheSize: # 캐시의 크기가 최대라면, 가장 오래된 도시이름 제거
-                    cache.pop(0)
-                cache.append(c)             
-                answer += 5                 # cache miss이므로 +5
-            else:                           # cache hit일 때 (도시 이름이 캐시 안에 있을 때)
-                cache.remove(c)             # 같은 도시 이름을 제거 
-                cache.append(c)            
-                answer += 1                 # cache hit이므로 +1
-                    
-        else:                               # 캐시크기가 없을 때, 저장 불가능
+    for city in cities:   
+        if city in cache:
+            cache.remove(city)
+            cache.append(city)
+            answer += 1
+        else:
+            if len(cache) == cacheSize:
+                cache.pop(0)
+            cache.append(city)
             answer += 5
-                
+    
     return answer
