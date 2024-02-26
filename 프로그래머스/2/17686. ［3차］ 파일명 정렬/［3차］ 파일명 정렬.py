345f397b99903c902e11e05f296a86461745187d
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2024-02-26 15:57:46 +0900

    [level 2] Title: [3차] 파일명 정렬, Time: 4.50 ms, Memory: 10.8 MB -BaekjoonHub

--- 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
+++ 프로그래머스/2/17686. ［3차］ 파일명 정렬/［3차］ 파일명 정렬.py
@@ -3,23 +3,24 @@ def solution(files):
     file_lst = []
     
     for file in files:
-        head, number, tail = '', '', ''             # HEAD, NUMBER, TAIL의 변수
+        head, number, tail = '', '', ''
         for i in range(len(file)):
-            if file[i].isdigit():                   # 파일명에 숫자가 남으면 숫자를 기준으로 구분
-                head = file[:i]                     # 숫자가 아닌 문자까지
-                number = file[i:]                   # 숫자부터 끝 문자열까지
-            
-                for j in range(len(number)):
-                    if not number[j].isdigit():     # number 숫자 구분
-                        tail = number[j:]           # 숫자가 아닌 문자부터 나머지까지
-                        number = number[:j]         # 숫자만 담기
-                        break
-                        
-                file_lst.append([head, number, tail])
+            if file[i].isdigit():
+                head = file[:i]
+                number = file[i:]
                 break
-    file_lst.sort(key = lambda x : (x[0].lower(), int(x[1])))    
+        
+        for i in range(len(number)):
+            if not number[i].isdigit():
+                tail = number[i:]
+                number = number[:i]
+                break
+        
+        file_lst.append([head, number, tail])
     
-    for i in file_lst:
-        answer.append(''.join(i))
+    file_lst.sort(key = lambda x : (x[0].lower(), int(x[1])))
     
+    for f in file_lst:
+        answer.append(''.join(f))
+                
     return answer
