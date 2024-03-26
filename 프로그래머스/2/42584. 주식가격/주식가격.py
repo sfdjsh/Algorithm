e2e1cef46b747ccc0e29d7d94c13dad7a81a013d
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2024-03-26 18:18:09 +0900

    [level 2] Title: 주식가격, Time: 33.51 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/2/42584. 주식가격/주식가격.py
@@ -0,0 +1,17 @@
+from collections import deque
+
+def solution(prices):
+    answer = []
+    
+    prices = deque(prices)
+    while prices:
+        price = prices.popleft()
+        
+        time = 0
+        for p in prices:
+            time += 1
+            if price > p:
+                break
+        answer.append(time)
+        
+    return answer
