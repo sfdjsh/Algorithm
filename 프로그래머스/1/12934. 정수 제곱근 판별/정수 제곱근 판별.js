72e2e26a8500c4f496054fc95bb91e5dfc58973f
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2024-11-22 08:46:52 +0900

    [level 1] Title: 정수 제곱근 판별, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
+++ 프로그래머스/1/12934. 정수 제곱근 판별/정수 제곱근 판별.js
@@ -1,7 +1,4 @@
-function solution(n) {
-    var answer = 0;
-    
-    sqrtNum = Math.sqrt(n)
-    
+function solution(n) {    
+    const sqrtNum = Math.sqrt(n)
     return Number.isInteger(sqrtNum) ? (sqrtNum + 1) ** 2 : -1;
 }
