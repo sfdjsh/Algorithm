b78054d339d728d88e872c8469eaacd45d363fc1
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2024-11-27 14:34:24 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.06 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -2,11 +2,8 @@ function solution(phone_number) {
     var answer = '';
     
     for (let i = 0; i < phone_number.length; i++) {
-        if (i < phone_number.length - 4) {
-            answer += '*';
-        } else {
-            answer += phone_number[i];
-        }
+        if (i < phone_number.length - 4) answer += '*';
+        else answer += phone_number[i];
     } 
     
     return answer;
