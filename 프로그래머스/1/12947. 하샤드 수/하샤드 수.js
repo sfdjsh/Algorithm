6522a25df7553f4c1e65648d67031f85896fa260
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2024-11-27 14:32:14 +0900

    [level 1] Title: 하샤드 수, Time: 0.10 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
+++ 프로그래머스/1/12947. 하샤드 수/하샤드 수.js
@@ -1,14 +1,8 @@
 function solution(x) {    
-    x = String(x)
-    var tmp = 0
-    for (let i = 0; i < x.length; i++) {
-        tmp += Number(x[i])    
-    }
+    x = String(x);
+    var tmp = 0;
+    for (let i = 0; i < x.length; i++) tmp += Number(x[i]);    
     
-    if (Number(x) % tmp) {
-        return false
-    } else {
-        return true
-    }    
+    return (Number(x) % tmp) ? false : true;
     
 }
