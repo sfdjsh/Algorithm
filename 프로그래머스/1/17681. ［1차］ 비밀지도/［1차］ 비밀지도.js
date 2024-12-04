25202ad72c5b993f15b1dd452736b9b84afc81cf
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2024-12-04 16:55:29 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.25 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.js
@@ -1,13 +1,16 @@
 function solution(n, arr1, arr2) {
-    var answer = [[]];
+    // var answer = [[]];
+    var answer = []
     for (let i = 0; i < n; i++) {
-        answer[i] = []
-        var bin_2 = (arr1[i] | arr2[i]).toString(2).padStart(n, 0)
-        for (let j = 0; j < bin_2.length; j++) {
-            if (bin_2[j] === '1') answer[i].push('#')
-            else answer[i].push(' ')
-        }
+        // answer[i] = []
+        // var bin_2 = (arr1[i] | arr2[i]).toString(2).padStart(n, 0)
+        // for (let j = 0; j < bin_2.length; j++) {
+        //     if (bin_2[j] === '1') answer[i].push('#')
+        //     else answer[i].push(' ')
+        // }
+        var bin_2 = (arr1[i] | arr2[i]).toString(2).padStart(n, 0).replace(/1/g,'#').replace(/0/g,' ')
+        answer.push(bin_2)
     }
-    answer = answer.map(e => e.join(''))
+    // answer = answer.map(e => e.join(''))
     return answer;
 }
