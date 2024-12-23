16ffe9ef871fbb142b28ffd2e274fb5ea2e85042
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2024-12-23 16:51:25 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.41 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.js
@@ -1,37 +1,31 @@
 function solution(new_id) {
-    var answer = '';
+    var answer = ''
+    answer = new_id.toLowerCase() // 1단계
     
-    // 1단계
-    new_id = new_id.toLowerCase();
+        .replace(/[^a-z0-9-_.]/g, '') // 2단계
+        /*
+            [^...] - [^] : 대괄호 안에서 쓰면 제외의 뜻, ex) [^a-z] : 소문자 알파벳을 제외한 모든 문자
+                   - 대괄호 안에서 -_. 같은 특수문자는 문자 그대로 해석     
+            a-z : 소문자 알파벳 (a부터 z까지)
+            0-9 : 숫자(0부터 9까지)
+            g : 모든 검색 결과를 반환        
+        */
     
-    // 2단계
-    const pos_str = /[a-zA-Z0-9\-_\.]/;
-    for (let str of new_id) {
-        if (pos_str.test(str)) answer += str;
-    }
+        .replace(/\.+/g, '.') // 3단계
+        /*
+            \. = 문자 그대로 해석
+            + = 바로 앞에 요소가 1번 이상 반복
+        */
+        .replace(/^\.|\.$/g, '') // 4단계
+        /*
+            ^ : 문자열의 시작
+            $ : 문자열의 끝
+            | : or 연산
+        */
+        .replace(/^$/, 'a') // 5단계
+        .slice(0, 15).replace(/\.$/, ''); // 6단계
     
-    // 3단계
-    let idx = 0
-    while (idx < answer.length - 1) {
-        if (answer[idx] === '.' && answer[idx + 1] === '.') answer = answer.slice(0, idx) + answer.slice(idx + 1);
-        else idx ++;
-    }
+    while (answer.length <= 2) answer += answer[answer.length - 1]; // 7단계
     
-    // 4단계
-    if (answer[0] === '.') answer = answer.slice(1);
-    if (answer[answer.length - 1] === '.') answer = answer.slice(0, answer.length - 1);
-    
-    // 5단계
-    if (!answer) answer += 'a';
-    
-    // 6단계
-    if (answer.length >= 16) {
-        answer = answer.slice(0, 15);
-        if (answer[answer.length - 1] === '.') answer = answer.slice(0, answer.length - 1);
-    }
-    
-    // 7단계
-    while (answer.length <= 2) answer += answer[answer.length - 1];
-  
-    return answer;
+    return answer
 }
