395f928e4f28775260ecb026f897bfa16f1e9e7b
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2025-01-11 00:34:57 +0900

    [level 1] Title: 햄버거 만들기, Time: 7251.34 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,20 @@
+function solution(ingredient) {
+    var answer = 0;
+    const bugger_order = [1, 2, 3, 1];
+    
+    let idx = 0
+    while (idx <= ingredient.length - 4) {
+        if (ingredient[idx] !== 1) idx++;
+        else {
+            const order = ingredient.slice(idx, idx + 4);
+            const order_filter = order.filter((ele, i) => ele !== bugger_order[i]).length;
+            if (!order_filter) {
+                ingredient.splice(idx, 4);
+                answer++;
+                idx = idx < 4 ? 0 : idx - 4
+            } else idx++;  
+        }
+    }
+    
+    return answer;
+}
