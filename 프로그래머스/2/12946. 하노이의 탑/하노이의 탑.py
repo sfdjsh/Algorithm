5525c66dcfdd0c671e8e2f8854cc41f0f539fee3
SUKHO JUNG <97646788+sfdjsh@users.noreply.github.com>
2025-02-27 19:39:00 +0900

    [level 2] Title: 하노이의 탑, Time: 9.12 ms, Memory: 18.5 MB -BaekjoonHub

---
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.py
@@ -0,0 +1,16 @@
+def solution(n):
+    answer = []
+    
+    def hanoi(n, start, end, via):
+        if n == 1:
+            answer.append([start, end])
+            return
+        
+        hanoi(n-1, start, via, end)
+        
+        answer.append([start, end])
+        
+        hanoi(n-1, via, end, start)
+        
+    hanoi(n, 1, 3, 2)
+    return answer
